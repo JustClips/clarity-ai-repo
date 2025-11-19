59445df7ec4dc91425e96dc5cbc65262718e77d2
JustClips <135924737+JustClips@users.noreply.github.com>
2025-11-19 15:27:48 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,23 @@
 
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const API_URL = 'https://YOUR-RAILWAY-BACKEND-DOMAIN.up.railway.app/graphql'; 
+
+const client = new ApolloClient({
+  uri: API_URL, 
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+);
