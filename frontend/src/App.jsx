eb99f7fb9427c878d0ccfc03047e91cd9d0a30d8
JustClips <135924737+JustClips@users.noreply.github.com>
2025-11-19 15:28:41 -0800

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import SignupForm from './SignupForm';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Welcome to ClarityAI</h1>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
